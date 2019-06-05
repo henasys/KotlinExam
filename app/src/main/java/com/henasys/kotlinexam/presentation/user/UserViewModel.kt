bfdfbe47514cc41f17ad217373c0b696e28b58f2
Alex Hojong Lee <alexhojonglee@gmail.com>
2019-06-05 17:44:47 +0900

    Rx test

--- app/src/main/java/com/henasys/kotlinexam/presentation/user/UserViewModel.kt
+++ app/src/main/java/com/henasys/kotlinexam/presentation/user/UserViewModel.kt
@@ -1,13 +1,16 @@
 package com.henasys.kotlinexam.presentation.user
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
 import com.henasys.kotlinexam.data.repository.UserRepository
 import com.henasys.kotlinexam.model.User
 import com.henasys.kotlinexam.presentation.Result
 import com.henasys.kotlinexam.presentation.common.mapper.toResult
 import com.henasys.kotlinexam.util.rx.SchedulerProvider
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import org.reactivestreams.Publisher
 import javax.inject.Inject
 
 class UserViewModel @Inject constructor(
@@ -17,7 +20,7 @@ class UserViewModel @Inject constructor(
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     val user: LiveData<Result<User>> by lazy {
-        repository.user.toResult(schedulerProvider).toLiveData()
+        toLiveData(repository.user.toResult(schedulerProvider))
     }
 
     override fun onCleared() {
@@ -26,5 +29,9 @@ class UserViewModel @Inject constructor(
     }
 
     fun start() {
+    }
+
+    fun toLiveData(source: Publisher<Result<User>>): LiveData<Result<User>> {
+        return LiveDataReactiveStreams.fromPublisher(source)
     }
 }
